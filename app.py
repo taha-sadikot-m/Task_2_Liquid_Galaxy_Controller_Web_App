9297940baa1c0b882208996698506e3bb3812e8e
Taha Sadikot <taha.sadikot.m@gmail.com>
2025-04-16 07:21:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -349,5 +349,5 @@ fi
         except Exception as e:
             print(f"Exception while relaunching screen {i}: {e}")
 
-if __name__ == '__main__'
+if __name__ == '__main__':
     app.run(host='0.0.0.0',port=7000)
